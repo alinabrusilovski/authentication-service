ba41a12d5cb541c2e621d2bfe958c7153dc6df6f
alinabrusilovski <alina.brusya@gmail.com>
2024-10-29 14:22:51 +0200

    Initial commit

---
+++ src/main/java/com/authservice/controller/HealthCheckController.java
@@ -0,0 +1,18 @@
+package com.authservice.controller;
+
+import com.authservice.dto.HealthStatusDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<HealthStatusDto> healthCheck() {
+        HealthStatusDto status = new HealthStatusDto("Application is running");
+        return ResponseEntity.ok(status);
+    }
+}
